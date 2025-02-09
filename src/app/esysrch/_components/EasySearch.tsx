4147851f4a9cb1d3a0fcf56ef661ecb0d4248964
chos1909@gmail.com <chos1909@gmail.com>
2025-02-10 08:30:22 +0900

    0210 임시 커밋

--- src/app/esysrch/_components/EasySearch.tsx
+++ src/app/esysrch/_components/EasySearch.tsx
@@ -1,14 +1,64 @@
 'use client'
 import React from 'react'
 import  styles from './easySearch.module.css';
+import Image from 'next/image';
+
+interface AIServiceCategory {
+  category: string;
+}
+
+const aiCategories: AIServiceCategory[] = [
+  { category: "Natural Language Processing" },
+  { category: "Computer Vision" },
+  { category: "Speech Recognition" },
+  { category: "Reinforcement Learning" },
+  { category: "Generative AI" },
+  { category: "Machine Learning Models" },
+  { category: "Recommendation Systems" },
+  { category: "AI Ethics & Fairness" },
+  { category: "Data Annotation & Labeling" },
+  { category: "AI Hardware Optimization" },
+  { category: "AI-Powered Search Engines" },
+  { category: "Robotics & Automation" },
+  { category: "AI for Healthcare" },
+  { category: "AI for Finance" },
+  { category: "AI for Cybersecurity" },
+  { category: "AI-Powered Chatbots" },
+  { category: "AI-Powered Virtual Assistants" },
+  { category: "AI for Autonomous Vehicles" },
+  { category: "AI for Supply Chain" },
+  { category: "AI for Gaming" },
+  { category: "AI for Marketing & Ads" }
+];
+
+
+console.log(aiCategories);
 
 export default function EasySearch() {
   return (
     <div className={styles.container}>
-      <div className={styles.esrchgrid}>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className={styles.esrch_container}>
+        <div className={`${styles.grid_container}`}>
+          <div className={styles.title_container}>
+            <div className={`${styles.title} titleM`}>Serive Name : Description and images</div>
+            <div className={`${styles.cntnts} bodyM`}>하단 카테고리를 선택하여 필요한 AI를 찾아보세요.</div>
+          </div>
+          <div className={styles.esrchgrid}>
+            {
+              aiCategories.map(e=>(
+              <div className={`${styles.grid_item}`}>
+                <div className={`titleM`}>{e.category}</div>
+                <div className={`${styles.categoryimg}`}><Image src={'/category_default.svg'} width={40} height={40} alt="defulatcate"/></div>
+              </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className={`${styles.result_container}`}>
+          <div>
+            adsadsads
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/app/home/_components/Home.tsx
+++ src/app/home/_components/Home.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import styles from './home.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 interface AIItem {
     category: string;
@@ -12,9 +14,9 @@ interface AIItem {
 
 export default function Home() {
     const [items, setItems] = useState([]);  // 데이터를 저장할 상태
+    const router = useRouter();
     // const [loading, setLoading] = useState<boolean>(true);  // 로딩 상태
     // const [error, setError] = useState<string | null>(null);  // 에러 상태
-    // const router = useRouter();
 
     useEffect(() => {
         const fetchItems = async () => {
@@ -63,51 +65,75 @@ export default function Home() {
         {
         items 
         ?
-        Object.entries(items).map(([category,items]) => (
+        Object.entries(items).map(([category,items],index) => (
             <div key={`${category}`} className={styles['grid-item']}>
             {/* Top Section */}
             <div className={styles['top-section']}>
-                {/* <img src={item.image} alt="Thumbnail" className={styles['top-image']} /> */}
+                <div className={styles.topimgdiv}>
+                    <Image src={'/category_default.svg'} alt="Thumbnail" className={styles['top-image']} width={32} height={32}/>
+                </div>
                 <span className={`${styles['top-text']} titleM`}>{`${category}`}</span>
             </div>
-            <div className={styles.mid}>
-            {/* Mid Section */}
-                {
-                    (items as AIItem[]).map((item,idx)=>
-                    <div
-                    // href={`/detail/${item.url}`}
-                    // href={`/detail/${item.name}`}
-                    key={`${item.name} ${idx}`}
-                    className={styles['mid-section']}
-                    // onClick={()=>router.push(`/detail/${item.name}`)} 
-                    >
-                        <Link 
-                        href={`${item.url}`}
-                        className={styles['mid-item']}
-                        target='_blank'
+            {/* 에디터픽 나올떄까지 임시 */}
+            <div className={`${index < 3 ? styles.midinner :''} `}>
+                <div className={`${styles.mid}`}>
+                {/* Mid Section */}
+                    {
+                        (items as AIItem[]).map((item,idx)=>
+                        <div
+                        // href={`/detail/${item.url}`}
+                        // href={`/detail/${item.name}`}
+                        key={`${item.name} ${idx}`}
+                        className={styles['mid-section']}
+                        onClick={()=>router.push(`/detail/${item.name}`)}
                         >
+                            {/* <Link 
+                            href={`${item.url}`}
+                            className={styles['mid-item']}
+                            target='_blank'
+                            > */}
                             <div 
-                                className={styles.midcontent}
+                            // href={`${item.url}`}
+                            className={styles['mid-item']}
+                            // target='_blank'
                             >
-                                <div className={`${styles.ranknum} ${item.order < 4 && styles.top3} titleS`}>{item.order}</div>
-                                {/* <img src={e.logo} alt="Logo" className={styles['mid-logo']} /> */}
-                                <span className='titleS'>{item.name}</span>
+                                <div 
+                                    className={styles.midcontent}
+                                >
+                                    <div className={`${styles.ranknum} ${item.order < 4 && styles.top3} titleS`}>{item.order}</div>
+                                    {/* <img src={e.logo} alt="Logo" className={styles['mid-logo']} /> */}
+                                    <span className='titleS'>{item.name}</span>
+                                </div>
+                                {/* 상위 3개만 노출 @todo 임시 */}
+                                {
+                                    index < 3 &&
+                                        <div className={styles.midcontent2}>
+                                            <div className={`${styles.ranknum}`} 
+                                            style={{backgroundColor:'transparent', color:'transparent'}}>{item.order}</div>
+                                            <div 
+                                            className='bodyM' 
+                                            style={{
+                                                fontWeight:400
+                                            }}># One line review</div>
+                                        </div>
+                                }
+                                {/* <div>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="ArrowSquareOut">
+                                    <g id="Vector">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M13.875 3.75C13.875 3.33579 14.2108 3 14.625 3H20.25C20.6642 3 21 3.33579 21 3.75V9.375C21 9.78921 20.6642 10.125 20.25 10.125C19.8358 10.125 19.5 9.78921 19.5 9.375V4.5H14.625C14.2108 4.5 13.875 4.16421 13.875 3.75Z" fill="black"/>
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M20.7803 3.21967C21.0732 3.51256 21.0732 3.98744 20.7803 4.28033L14.0303 11.0303C13.7374 11.3232 13.2626 11.3232 12.9697 11.0303C12.6768 10.7374 12.6768 10.2626 12.9697 9.96967L19.7197 3.21967C20.0126 2.92678 20.4874 2.92678 20.7803 3.21967Z" fill="black"/>
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M3.43934 6.43934C3.72064 6.15804 4.10217 6 4.5 6H10.5C10.9142 6 11.25 6.33579 11.25 6.75C11.25 7.16421 10.9142 7.5 10.5 7.5L4.5 7.5L4.5 19.5H16.5V13.5C16.5 13.0858 16.8358 12.75 17.25 12.75C17.6642 12.75 18 13.0858 18 13.5V19.5C18 19.8978 17.842 20.2794 17.5607 20.5607C17.2794 20.842 16.8978 21 16.5 21H4.5C4.10218 21 3.72065 20.842 3.43934 20.5607C3.15804 20.2794 3 19.8978 3 19.5V7.5C3 7.10217 3.15804 6.72064 3.43934 6.43934Z" fill="black"/>
+                                    </g>
+                                    </g>
+                                    </svg>
+                                </div> */}
+                            {/* </Link> */}
                             </div>
-                            {/* <div>
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g id="ArrowSquareOut">
-                                <g id="Vector">
-                                <path fillRule="evenodd" clipRule="evenodd" d="M13.875 3.75C13.875 3.33579 14.2108 3 14.625 3H20.25C20.6642 3 21 3.33579 21 3.75V9.375C21 9.78921 20.6642 10.125 20.25 10.125C19.8358 10.125 19.5 9.78921 19.5 9.375V4.5H14.625C14.2108 4.5 13.875 4.16421 13.875 3.75Z" fill="black"/>
-                                <path fillRule="evenodd" clipRule="evenodd" d="M20.7803 3.21967C21.0732 3.51256 21.0732 3.98744 20.7803 4.28033L14.0303 11.0303C13.7374 11.3232 13.2626 11.3232 12.9697 11.0303C12.6768 10.7374 12.6768 10.2626 12.9697 9.96967L19.7197 3.21967C20.0126 2.92678 20.4874 2.92678 20.7803 3.21967Z" fill="black"/>
-                                <path fillRule="evenodd" clipRule="evenodd" d="M3.43934 6.43934C3.72064 6.15804 4.10217 6 4.5 6H10.5C10.9142 6 11.25 6.33579 11.25 6.75C11.25 7.16421 10.9142 7.5 10.5 7.5L4.5 7.5L4.5 19.5H16.5V13.5C16.5 13.0858 16.8358 12.75 17.25 12.75C17.6642 12.75 18 13.0858 18 13.5V19.5C18 19.8978 17.842 20.2794 17.5607 20.5607C17.2794 20.842 16.8978 21 16.5 21H4.5C4.10218 21 3.72065 20.842 3.43934 20.5607C3.15804 20.2794 3 19.8978 3 19.5V7.5C3 7.10217 3.15804 6.72064 3.43934 6.43934Z" fill="black"/>
-                                </g>
-                                </g>
-                                </svg>
-                            </div> */}
-                        </Link>
-                    </div>
-                    )
-                }
+                        </div>
+                        )
+                    }
+                </div>
             </div>
 
             {/* Bottom Section */}
